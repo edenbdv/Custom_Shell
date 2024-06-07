34533af092c37f289681be30c7b5083a52aa1915
Eden Ben David <edenbdv1@gmail.com>
2024-06-07 19:24:17 +0300

    add tester

--- test_script.sh
+++ test_script.sh
@@ -1,12 +1,12 @@
+
 #!/bin/bash
 
-# Create a test file with 100 commands
-for i in {1..100}; do
+# Create a test file with 101 commands
+for i in {1..101}; do
   echo "echo Command $i" >> test_commands.txt
 done
 
-# Add one more command to exceed the history limit
-echo "echo Command 101" >> test_commands.txt
-
 # Run the shell program and pass the test file as input
 ./part3.out < test_commands.txt
+
+./part3.out history
